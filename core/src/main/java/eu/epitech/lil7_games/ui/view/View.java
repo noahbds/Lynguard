5b312c0c66e95f354cb1f37cbeef0a4545881f82
Unrectified <nathanael.michau@epitech.eu>
2025-11-27 15:08:00 +0100

    edit: redma

--- core/src/main/java/eu/epitech/lil7_games/ui/view/View.java
+++ core/src/main/java/eu/epitech/lil7_games/ui/view/View.java
@@ -1,17 +1,19 @@
 package eu.epitech.lil7_games.ui.view;
 
+import java.beans.PropertyChangeListener;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Array;
-import eu.epitech.lil7_games.ui.model.ViewModel;
 
-import java.beans.PropertyChangeListener;
+import eu.epitech.lil7_games.ui.model.ViewModel;
 
 public abstract class View<T extends ViewModel> extends Table {
     protected final Stage stage;
